33920add6da63a8a238a095807500913cc0312d8
poskcmit <57031936+poskcmit@users.noreply.github.com>
2019-12-07 11:50:50 +0300

    Update run-word.ino

--- run-word.ino
+++ run-word.ino
@@ -2,12 +2,12 @@
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 char detectButton(int portValue) {
-  if(portValue <= 80) return "R";
-  if(portValue > 80 && portValue <= 200) return "U";
-  if(portValue > 200 && portValue <= 400) return "D";
-  if(portValue > 400 && portValue <= 600) return "L";
-  if(portValue > 600 && portValue <= 800) return "S";
-  return "N";
+  if(portValue <= 80) return 'R';
+  if(portValue > 80 && portValue <= 200) return 'U';
+  if(portValue > 200 && portValue <= 400) return 'D';
+  if(portValue > 400 && portValue <= 600) return 'L';
+  if(portValue > 600 && portValue <= 800) return 'S';
+  return 'N';
 }
 
 void setup() {
@@ -16,14 +16,14 @@ void setup() {
 }
 
 int row = 0;
-int column = 0
+int column = 0;
 
 void loop() {
   int buttonValue = analogRead(A0);
   if(buttonValue < 900) {
     char button = detectButton(buttonValue);
-    if(button == "R") column++;
-    if(button == "L") column--;
+    if(button == 'R') column++;
+    if(button == 'L') column--;
   }
   lcd.setCursor(column, row);
   lcd.print("A");
