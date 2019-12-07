4a24a4034d50c1e798f0bbbc0083b881c435f769
poskcmit <57031936+poskcmit@users.noreply.github.com>
2019-12-07 11:26:22 +0300

    Update run-word.ino

--- run-word.ino
+++ run-word.ino
@@ -1,14 +1,22 @@
 #include <LiquidCrystal.h>
-LiquidCrysal lcd(8, 9, 4, 5, 6, 7);
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 void setup() {
   lcd.begin(16, 2);
+  pinMode(A0, INPUT);
 }
 
-int row;
-int column;
+int row = 0;
+int column = 0;
+int buttonValue = 0;
 
 void loop() {
+  buttonValue = analogRead(A0);
+  if(buttonValue < 900) {
+    column++;
+  }
   lcd.setCursor(column, row);
   lcd.print("A");
+  delay(250);
+  lcd.clear();
 }
