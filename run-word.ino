d48ed9c6b266afb1d2a8f778bc1dc7f1fa9e2b60
poskcmit <57031936+poskcmit@users.noreply.github.com>
2019-12-07 11:43:58 +0300

    Update run-word.ino

--- run-word.ino
+++ run-word.ino
@@ -1,19 +1,29 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
+char detectButton(int portValue) {
+  if(portValue <= 80) return "R";
+  if(portValue > 80 && portValue <= 200) return "U";
+  if(portValue > 200 && portValue <= 400) return "D";
+  if(portValue > 400 && portValue <= 600) return "L";
+  if(portValue > 600 && portValue <= 800) return "S";
+  return "N";
+}
+
 void setup() {
   lcd.begin(16, 2);
   pinMode(A0, INPUT);
 }
 
 int row = 0;
-int column = 0;
-int buttonValue = 0;
+int column = 0
 
 void loop() {
-  buttonValue = analogRead(A0);
+  int buttonValue = analogRead(A0);
   if(buttonValue < 900) {
-    column++;
+    char button = detectButton(buttonValue);
+    if(button == "R") column++;
+    if(button == "L") column--;
   }
   lcd.setCursor(column, row);
   lcd.print("A");
