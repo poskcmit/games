d3cbefd8cb8ab17e2dbf72eaec45b55080dda9a4
poskcmit <57031936+poskcmit@users.noreply.github.com>
2019-12-14 11:31:34 +0300

    Create wall.ino

---
+++ wall.ino
@@ -0,0 +1,19 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+void setup() {
+  lcd.begin(16, 2);
+}
+
+int wallColumn = 0;
+const int wallRow = 2;
+
+void loop() {
+  lcd.setCursor(wallColumn, wallRow);
+  delay(200);
+  wallColumn--;
+  if(wallColumn == 0) {
+    wallColumn = 16;
+  }
+  delay(random(100, 1000));
+}
