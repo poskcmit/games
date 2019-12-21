9758100480d8b92a987e6b2dd1dc04abdc72afd7
poskcmit <57031936+poskcmit@users.noreply.github.com>
2019-12-21 10:34:24 +0300

    Update dino_game.ino

--- dino_game.ino
+++ dino_game.ino
@@ -1,6 +1,15 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
+char detectButton(int portValue) {
+  if(portValue <= 80) return 'R';
+  if(portValue > 80 && portValue <= 200) return 'U';
+  if(portValue > 200 && portValue <= 400) return 'D';
+  if(portValue > 400 && portValue <= 600) return 'L';
+  if(portValue > 600 && portValue <= 800) return 'S';
+  return 'N';
+}
+
 byte suit_1[8] = {
   B01110,
   B10001,
@@ -27,16 +36,29 @@ void setup() {
   lcd.createChar(0, suit_1);
   lcd.createChar(1, suit_2);
   lcd.begin(16, 2);
+  pinMode(A0, INPUT);
 }
 
 int suit = 0;
+int jeffRow = 0;
+int jeffColumn = 0;
 
 void loop() {
-  lcd.setCursor(2, 2);
-  lcd.write(byte(suit));
-  delay(300);
+  int buttonValue = analogRead(A0);
+  if(buttonValue < 900) {
+    char button = detectButton(buttonValue);
+    if(button == 'R') jeffColumn++;
+    if(button == 'L') jeffColumn--;
+    if(button == 'U') jeffRow++;
+    if(button == 'D') jeffRow--;
+  }
   suit++;
   if(suit > 1) {
     suit = 0;
   }
+  
+  lcd.setCursor(jeffColumn, jeffRow);
+  lcd.write(byte(suit));
+  delay(250);
+  lcd.clear();
 }
