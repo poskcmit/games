21ef750a1d7f90adc3ac4470310e397fa268d8c5
poskcmit <57031936+poskcmit@users.noreply.github.com>
2020-04-04 11:29:16 +0300

    Update lesson10.ino

--- lesson10.ino
+++ lesson10.ino
@@ -1,21 +1,26 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+#define RetroEclairage 10
 
-char detectButton(int portValue) {
-  if(portValue <= 80) return 'R';
-  else if(portValue <= 200) return 'U';
-  else if(portValue <= 400) return 'D';
-  else if(portValue <= 600) return 'L';
-  else if(portValue <= 800) return 'S';
-  return 'N';
+char detectButton(int analog) {
+  if (analog < 50) return 'R';
+  if ( (analog > 95) && (analog < 150) ) return 'U';
+  if ( (analog > 240) && (analog < 350) ) return 'D';
+  if ( (analog > 400) && (analog < 500) ) return 'L';
+  if ( (analog > 600) && (analog < 750) ) return 'S';
 }
 
 void setup() {
   lcd.begin(16, 2);
-  pinMode(A0, INPUT);
+  pinMode(RetroEclairage, OUTPUT);
+  digitalWrite(RetroEclairage, HIGH);
+  delay(100);
+  digitalWrite(RetroEclairage, LOW);
 }
+
 int x = 0;
 int y = 0;
+
 void loop() {
   int buttonValue = analogRead(A0);
   if(buttonValue < 900) {
@@ -28,6 +33,6 @@ void loop() {
   
   lcd.setCursor(x, y);
   lcd.print("A");
-  lcd.delay(100);
+  delay(100);
   lcd.clear();
 }
